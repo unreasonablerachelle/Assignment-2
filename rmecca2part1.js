4e15f496349180028b0869f7500c9ec3ac8efb4e
Brett Russell <ytotre@gmail.com>
2017-03-13 23:15:13 -0400

    stuff and things

--- rmecca2part1.js
+++ rmecca2part1.js
@@ -1,47 +1,45 @@
-var bodyArt=["elbow","arm","face","nose","hair"];
-var bodyparts
+var bodyParts = [ "elbow", "arm", "face", "nose", "hair" ];
 
-var adjectiveIn=["gross","crooked","scaley","rubber","smelly"];
-var animalIn=["crocodile","snake","platypus","walrus","cow"];
-var adjectiveComp=["pretty","soft","smart","warm","artistic"];
-var animalComp=["cat","dog","dolphin","parrot","flamingo"];
-
+var adjectiveIn = [ "gross", "crooked", "scaley", "rubber", "smelly" ];
+var animalIn = [ "crocodile", "snake", "platypus", "walrus", "cow" ];
+var adjectiveComp = [ "pretty", "soft", "smart", "warm", "artistic" ];
+var animalComp = [ "cat", "dog", "dolphin", "parrot", "flamingo" ];
 
 
-function box (){ //text box
-	localStorage.setItem("person");
-	document.getElementById("aperson").innerHTML=localStorage.getItem("person");
+// Stores the value in localStorage
+function storeValue(elemName, value) {
+  localStorage.setItem("stored_" + elemName, value);
 }
 
-function aperson(){ //name to output from box
-	
-	
-}
-function fillInInsultOrComp (){
-		var bodyparts=bodyArt[Math.floor(Math.random()*bodyArt.length)];
-		bodyArt=document.getElementById("bodyPart");
-	
-	if ("doit" == "insult"){ //insult generator
-		var adjectIn
-		var animIn
-		var adjectIn=adjectiveIn=[Math.floor(Math.random()*adjectiveIn.length)];
-		adjectiveIn=document.getElementById("adjective");
-	
-		var animIn=animalIn[Math.floor(Math.random()*animalIn.length)];
-		animalIn=document.getElementById("animal");
-		alert("is this working?");
-		// button onload?
-	} 
-	else("doit" == "compliment") { //compliment generator
-		var adjectCo
-		var aniCo
-	
-		var adjectCo=adjectiveComp[Math.floor(Math.random()*adjectiveComp.length)];
-		adjectiveComp=document.getElementById("adjective");
-	
-		var aniCo=animalComp=[Math.floor(Math.random()*animalComp.length)];
-		animalComp=document.getElementById("animal");
-		//button onload?
+function replaceElement(elemName) {
+  var elem = document.getElementById(elemName);
+  elem.innerHTML = localStorage.getItem("stored_" + elemName);
 }
 
+function randArrayItem(array) {
+  return array[ Math.floor(Math.random() * array.length)];
+}
+
+function p2_fillInPage() {
+  replaceElement("userName");
+  replaceElement("bodyPart");
+  replaceElement("adjective");
+  replaceElement("animal");
+}
+
+function p1_genPhrase() {
+ storeValue("userName", document.getElementById("userName").value);
+ storeValue("bodyPart", randArrayItem(bodyParts));
+}
+
+function p1_genInsult() {
+ p1_genPhrase();
+ storeValue("adjective", randArrayItem(adjectiveIn));
+ storeValue("animal", randArrayItem(animalIn));
+}
+
+function p1_genCompliment() {
+ p1_genPhrase();
+ storeValue("adjective", randArrayItem(adjectiveComp));
+ storeValue("animal", randArrayItem(animalComp));
 }
